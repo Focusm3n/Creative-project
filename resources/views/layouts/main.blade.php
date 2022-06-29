2cd370344736973e9e1a70500314db1102e2bc99
Focusm3n <ilya.k007123@gmail.com>
2022-06-29 15:57:05 +0300

    add bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,20 +5,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>@yield('title')</title>
 </head>
 <body>
-<div>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{ route('main.index') }}">Main</a></li>
-                <li><a href="{{ route('post.index') }}">Posts</a></li>
-                <li><a href="{{ route('about.index') }}">About</a></li>
-                <li><a href="{{ route('contact.index') }}">Contacts</a></li>
-            </ul>
-        </nav>
-    </div>
+<div class="container">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('main.index') }}">Main</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
     @yield('content')
 </div>
 </div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,11 +3,24 @@
 @section('title', 'Объявления')
 
 @section('content')
-    @foreach($posts as $post)
-
-        <div>{{ $post->title }}</div>
-        <div>{{ $post->content }}</div>
-        <div>{{ $post->likes }}</div>
-        <br>
-    @endforeach
+    <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Название</th>
+            <th scope="col">Содержание</th>
+            <th scope="col">Кол-во лайков</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($posts as $post)
+            <tr>
+                <th scope="row">{{ $post->id }}</th>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->content }}</td>
+                <td>{{ $post->likes }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
